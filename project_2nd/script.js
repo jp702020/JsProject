ae110b45da43fbbcef0e7bd782abd486910680dc
Patel Junaid <jp70202021@gmail.com>
2024-06-03 22:51:20 +0530

    third project

--- project_2nd/script.js
+++ project_2nd/script.js
@@ -6,14 +6,15 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   const height = document.getElementById("height").value / 100;
   const weight = document.getElementById("weight").value;
+  const result = document.getElementById("results");
   if (height != "" || weight != "") {
     // console.log(height.value / 100);
     // console.log(weight.value);
 
     let bmi = (weight / (height * height)).toFixed(1);
 
-    const result = document.getElementById("results");
-
     result.innerText = bmi;
+  } else {
+    result.innerText = "please give a vild height or weight";
   }
 });
